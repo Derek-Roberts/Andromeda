efc6b0d03c016b5cdd2637d715ea668b90b1a741
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-07-10 17:25:58 -0700

    Testing basic api request with a sample URL.  Working exactly as I'd hoped!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    #@response = HTTParty.get('http://www.theverge.com/2016/7/10/12127638/uber-ergo-investigation-lawsuit-fraud-travis-kalanick')
+    @article_url = "http://www.theverge.com/2016/7/10/12127638/uber-ergo-investigation-lawsuit-fraud-travis-kalanick"
+    @response = HTTParty.get("http://api.smmry.com/&SM_API_KEY=A252EB9549&SM_LENGTH=5&SM_URL=#{@article_url}")
+    @character_count = @response["sm_api_character_count"]
+    @article_title = @response["sm_api_title"]
+    @summary = @response["sm_api_content"]
   end
 end
